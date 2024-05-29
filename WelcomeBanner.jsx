04ac7001e139a451b882b6a951eb303d88defd1f
tebohomanyeli <115980720+tebohomanyeli@users.noreply.github.com>
2024-05-29 04:00:36 +0200

    Create WelcomeBanner.jsx
    first commit

---
+++ WelcomeBanner.jsx
@@ -0,0 +1,18 @@
+Import Image from 'next/image'
+Import React from 'react'
+
+Function WelcomeBanner() {
+return ( 
+
+<div className='flex gap-5 items-center Wbg-white rounded-xl p-5'>
+
+  <Image src='/panda.png' alt='panda" width=(100) height=(100)/>
+
+  <div>
+  <h2>
+    Welcome to TubeGuruju Academy
+  </h2> 
+
+  <h2>
+    Explore, Learn and Build All Real Life Projects
+  </h2>
